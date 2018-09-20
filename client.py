8da69bf6cb1c1c134685b2e0a1d8ea0efa6263fd
srasool2 <33038520+srasool2@users.noreply.github.com>
2018-09-19 23:54:16 -0500

    Python Socket Programming

---
+++ client.py
@@ -0,0 +1,23 @@
+import socket
+
+
+def client():
+    '''
+    function to define the client behaviour.
+    '''
+    client_socket = socket.socket() # creating the socket object.
+    client_socket.connect((socket.gethostname(), 5000)) # connecting to the socket object.
+    client_str = "Enter Input : "
+    message = raw_input(client_str) # Take the input from the user. 
+
+    while message.lower().strip() != 'bye': 
+        client_socket.send(message) # send the message given by client to server.
+        data = client_socket.recv(1024) # receive the response from the server.
+        print('Received from server: ' + data) # show what is received from the server.
+        message = raw_input(client_str) # keep taking the input unless client enter bye.
+
+    client_socket.close() # close the socket connection.
+
+
+if __name__ == '__main__':
+    client()
---
+++ server.py
@@ -0,0 +1,27 @@
+import socket
+
+def server():
+    '''
+    function to define the server behaviour.
+    '''
+    server_socket = socket.socket() # creating the socket object.
+    server_socket.bind((socket.gethostname(), 5000)) # bind to the socket object on port 5000 .
+
+    server_socket.listen(1) # list on the socket.
+    connection, address = server_socket.accept() # accept the client connection
+    print("Connection from: " + str(address)) # print the details of the client.
+    while True:
+        data = connection.recv(1024) # receive the data from the client
+        if not data: # if no data break.
+            break
+        print("Client Sent: " + str(data)) # show data sent by client.
+        if str(data).lower() == "hi":
+            resp = "hello"
+        else:
+            resp = "Goodbye"
+        connection.send(resp) # send back the response.
+    connection.close()
+
+
+if __name__ == '__main__':
+    server()
